ecc4c90f33ef7e6e32a1a5200b348e88ba9a1b1c
chiragnarang3003 <chirag82888@gmail.com>
2018-08-17 18:47:56 +0530

    updated file

---
+++ assignment2.py
@@ -0,0 +1,37 @@
+#Question1:->Print anything you want on screen?
+print("hello,This is my assignment2. ");
+
+#Question2:->Join 2 strings using '+'?
+str1='Chirag '
+str2='Narang'
+print(str1+str2)
+#or
+print('Chirag','Narang')
+
+#Question3:->Take the input of 3 variables and print their values on screen?
+x=input("Enter value of x: ")
+y=input("Enter value of y: ")
+z=input("Enter value of z: ")
+print("value of x is:",x)
+print("value of y is:",y)
+print("value of z is:",z)
+
+#Question4:->print "Let's get started" on screen?
+print('''"Let's get started"''')
+
+#Question5:->Print the following values using placeholder.
+s="Acadview"
+course="Python"
+fees=5000
+print(s,course,fees)
+
+#Question6(A):->Interesting exercise?
+name="Tony Stark"
+salary=1000000
+print( '%s ' '%d' %(name,salary))
+
+#Question6:-> Find the area of circle pi = 3.14 Take radius as input from user Print the area of circle 
+radius=int(input("Enter the radius of a circle : "))
+pi=3.14
+result=pi*radius*radius
+print("area of the circle is :",result)
